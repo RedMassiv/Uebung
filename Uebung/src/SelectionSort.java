497a399868d1d76ab3ab93fe313e47193e354de5
Marek.Czernohous <Marek.Czernohous@SK-B4B52FACDE69.2631.skool.net>
2017-02-15 15:30:19 +0100

    Dokumentation hinzugefügt

--- Uebung/src/SelectionSort.java
+++ Uebung/src/SelectionSort.java
@@ -2,22 +2,22 @@ import java.lang.reflect.Array;
 
 public class SelectionSort {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) {   // Hauptmethode
 		
-		int[] list = {5, 8, 3, 6, 8, 9, 4, 2,10, 574,0,5};
-		sortArray(list);
+		int[] list = {5, 8, 3, 6, 8, 9, 4, 2,10, 574,0,5};   // Array wird angelegt
+		sortArray(list);									 // Array wird an die Funktion "SortArray" übergeben
 		
 
 		}
 	
 	
 	public static void sortArray(int[] array){
-		for(int i=0;i<array.length;i++){
+		for(int i=0;i<array.length;i++){     // für jedes Feld des Arrays
 	           
-	           output(array);
-	           int minimum = findMinimum(array,i,array.length);
+	           output(array);				 // erstmal das ganze Array ausgeben, sieht schön aus (siehe Funktion "output")
+	           int minimum = findMinimum(array,i,array.length);  // mit der Funktion "findMinimum" wir die Position der kleinsten Zahl ermittelt und nach "minimum" geschrieben
 	           
-	           if(i!=minimum){
+	           if(i!=minimum){				// wenn eine Zahl gefunden wird, die Kleiner als die, mit der verglichen wird, gefunden wird, werden die Zahlen in der swap Methode getauscht
 	           swap(array,i,minimum);
 	           }
 	       }
@@ -26,7 +26,7 @@ public class SelectionSort {
 	
 	
 	public static int swap(int[] array, int position1, int position2) {
-		int tmp = array[position2];
+		int tmp = array[position2]; 		//zwei Zahlen des Array werden getauscht.
 		array[position2]=array[position1];
 		array[position1]=tmp;
 		return position1;
@@ -41,7 +41,7 @@ public class SelectionSort {
 	       
 	       for(int i=startIndex+1;i<endIndex;i++){
 	           
-	           if(array[i]<currentMinimum){
+	           if(array[i]<currentMinimum){    //die eigentliche Magie: Finde die kleinste Zahl im Array
 	               position = i;
 	               currentMinimum=array[i];
 	           }
@@ -54,7 +54,7 @@ public class SelectionSort {
 	
 	
 	public static boolean isSorted(int[] array) {
-		
+													// Das müsste noch implementiert werden!
 		return true;
 		}
 
