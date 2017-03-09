d89006d3eabf9a47419b1025412284195f5abfe1
Marek.Czernohous <Marek.Czernohous@SK-D8CB8A4FAB08.2631.skool.net>
2017-03-09 09:15:57 +0100

    Initial commit

---
+++ Uebung/src/Hanoi_2017.java
@@ -0,0 +1,26 @@
+public class Hanoi_2017 {
+
+	
+	
+	public static void main(String[] args) { 
+		
+		int n = 9;
+		int von = n;
+		int ueber = 0;
+		int nach = 0;
+		
+	}
+		
+	public void transportiereTurm(int von, int ueber, int nach){
+		
+		
+	}
+}
+		
+	
+
+	
+	
+	
+ 
+
--- Uebung/src/SelectionSort.java
+++ Uebung/src/SelectionSort.java
@@ -1,32 +1,78 @@
+import java.util.Arrays;
 public class SelectionSort {
 
+	
 static int counter=0;
 
 	public static void main(String[] args) {   // Hauptmethode
-		int n=1000; //Anzahl der Arrayfelder
+		int n=10000; //Anzahl der Arrayfelder
 		int list[] = new int[n];   // Array wird angelegt mit der richtigen Anzahl an Feldern
 		for (int i=0; i<n; i++){
 			double random=Math.random()*1000;
 			list[i]=(int)random;
 		}
-		
-		
+		int list2[] = Arrays.copyOf(list, list.length);
+				
 		output(list);
-		sortArray(list);									    // Array wird an die Funktion "SortArray" übergeben
+		counter=0;
+		System.out.println();
+		
+		selectionSortArray(list);									    // Array wird an die Funktion "SortArray" übergeben
+	    
+		
 		System.out.println("Sortiervorgänge: "+counter);
+		System.out.println();
+		counter=0;
+		bubbleSort(list2);
+		System.out.println("Sortiervorgänge: "+counter);
+		System.out.println();
+		counter=0;
 		
 		}
 	
 	
-	public static void sortArray(int[] array){
+	
+	public static void bubbleSort(int[] list){
+		System.out.println("BubbleSort");
+		int length = list.length-1;
+		int temp=0;
+		
+	for(int j = 0; j<length; length--){	
+		
+		for(int i =0; i<length; i++){
+			
+			if(list[i]>list[i+1]){
+				temp = list[i];
+				list[i] = list[i+1];
+				list[i+1] = temp;
+				 counter++;
+			//	output(list);
+			   
+			}
+		}
+			
+			
+			
+		}
+	
+		
+		
+		
+	}
+	
+	
+	public static void selectionSortArray(int[] array){
+		
+		System.out.println("SelectionSort");
 		
 		for(int i=0;i<array.length;i++){     // für jedes Feld des Arrays
 	           
 	               int minimum = findMinimum(array,i,array.length);  // mit der Funktion "findMinimum" wir die Position der kleinsten Zahl ermittelt und nach "minimum" geschrieben
 	           
 	           if(i!=minimum){				// wenn eine Zahl gefunden wird, die Kleiner als die, mit der verglichen wird, gefunden wird, werden die Zahlen in der swap Methode getauscht
-	           swap(array,i,minimum);
-	           output(array);				 // erstmal das ganze Array ausgeben, sieht schön aus (siehe Funktion "output")
+	        	   swap(array,i,minimum);
+	               counter++;
+	        	//   output(array);				 // erstmal das ganze Array ausgeben, sieht schön aus (siehe Funktion "output")
 		       
 	           }
 	       }
@@ -73,6 +119,7 @@ static int counter=0;
 			i++;
 		}
 		// Das müsste noch implementiert werden!
+		
 		return sorted;
 		}
 
@@ -83,7 +130,7 @@ static int counter=0;
 			System.out.print(array[i]+",");
 			}
 		System.out.println("sortiert:"+ isSorted(array));
-		counter++;
+	//	counter++;
 	}
 	
 }
